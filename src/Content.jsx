7a796ae7c017804402d5d1b9629b853761f67c30
Anna <annamtoman@gmail.com>
2023-04-17 11:59:10 -0500

    adds post data

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,15 @@
+import { PostsIndex } from "./PostsIndex"
+
 export function Content() {
+  const posts = [
+    {id: 1, 
+     title: "Focused eco-centric artificial intelligence", body: "Diy park selvage everyday. Letterpress banh mi marfa retro 90's migas. Pug sriracha craft beer. Irony semiotics echo polaroid truffaut. 90's green juice whatever pug asymmetrical roof. Flexitarian cliche cardigan single-origin coffee meh brooklyn bicycle rights fixie. Cardigan yr photo booth wolf green juice everyday.", 
+     image: "https://cdn.britannica.com/09/189409-050-01172C19/Cast-Jason-Alexander-Seinfeld-Michael-Richards.jpg"},
+  ];
+
     return (
       <div>
-        <h1>Welcome to React!</h1>
+        <PostsIndex posts={posts} />
       </div>
     )
   }
